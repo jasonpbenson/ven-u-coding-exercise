28e9bc6d87db33fca6ddd6d0a372c0e2455c9927
jasonpbenson <jasonbenson3rd@gmail.com>
2020-03-02 18:32:20 -0500

    Updates

---
+++ precache-manifest.a576bb9d3206edf2dd01e81fbf628012.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34efce8536161effde8622674f07bf04",
+    "url": "/ven-u-coding-exercise/index.html"
+  },
+  {
+    "revision": "85be85bae0611834b5dc",
+    "url": "/ven-u-coding-exercise/static/css/main.f1dae865.chunk.css"
+  },
+  {
+    "revision": "7d6c66cb1240fd46fefb",
+    "url": "/ven-u-coding-exercise/static/js/2.fa3d6552.chunk.js"
+  },
+  {
+    "revision": "0bdc4f0d8b3d84ae6a73bb9902b479ee",
+    "url": "/ven-u-coding-exercise/static/js/2.fa3d6552.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85be85bae0611834b5dc",
+    "url": "/ven-u-coding-exercise/static/js/main.5fdd0988.chunk.js"
+  },
+  {
+    "revision": "cb6670fe69546b9ca28b",
+    "url": "/ven-u-coding-exercise/static/js/runtime-main.2d77abeb.js"
+  },
+  {
+    "revision": "501ccf4f360332121d995cd5d8223a9a",
+    "url": "/ven-u-coding-exercise/static/media/error-flag.501ccf4f.svg"
+  },
+  {
+    "revision": "624a3b9d47d8835e61ceb503870a7672",
+    "url": "/ven-u-coding-exercise/static/media/hacker-news-logo.624a3b9d.svg"
+  },
+  {
+    "revision": "82f7d036e6a75188c18e67b706071279",
+    "url": "/ven-u-coding-exercise/static/media/link-icon.82f7d036.svg"
+  }
+]);
